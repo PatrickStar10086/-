5ca8682bf7f1e0fab73ef8ffa6e52a7db0142d59
PatrickStar10086 <paforstu0213@xauat.edu.cn>
2024-02-26 21:58:10 +0800

    作业提交

---
+++ user/src/com/jun/entity/Movie.java
@@ -0,0 +1,25 @@
+package com.jun.entity;
+
+public class Movie implements Comparable<Movie> {
+    public String movieName;
+    public int score;
+
+    public Movie(String movieName, int score) {
+        this.movieName = movieName;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Movie{" +
+                "movieName='" + movieName + '\'' +
+                ", score=" + score +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Movie o) {
+        return score > o.score ? -1 : 1;
+    }
+
+}
